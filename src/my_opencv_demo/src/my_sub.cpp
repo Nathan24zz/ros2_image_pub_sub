0e2cb48f5b2f9084cafb9afb7e45cdd1c44987e9
Nathan24zz <nathanterbaik@gmail.com>
2023-04-10 21:48:30 +0700

    refactor: change folder to src

--- src/my_opencv_demo/src/my_sub.cpp
+++ src/my_opencv_demo/src/my_sub.cpp
@@ -6,6 +6,8 @@
 #include "std_msgs/msg/string.hpp"
 #include "sensor_msgs/msg/image.hpp"
 #include "my_opencv_demo_interfaces/msg/image_with_time.hpp"
+#include <opencv2/opencv.hpp>
+#include <cv_bridge/cv_bridge.h>
 
 using std::placeholders::_1;
 
@@ -25,6 +27,19 @@ private:
         auto current_image = *msg;
         uint64_t ms = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
         RCLCPP_INFO(this->get_logger(), "get_image: %i ms", ms - current_image.time);
+        cv_bridge::CvImagePtr cv_ptr;
+        try
+        {
+            cv_ptr = cv_bridge::toCvCopy(current_image.image, sensor_msgs::image_encodings::BGR8);
+            // cv_bridge::toCvShare(msg, enc::BGR8);
+            cv::imshow("view", cv_ptr->image);
+            cv::waitKey(10);
+        }
+        catch (const cv_bridge::Exception &e)
+        {
+            auto logger = rclcpp::get_logger("my_subscriber");
+            RCLCPP_ERROR(logger, "Could not convert to 'bgr8'.");
+        }
     }
     rclcpp::Subscription<my_opencv_demo_interfaces::msg::ImageWithTime>::SharedPtr subscription_;
 };
@@ -32,6 +47,8 @@ private:
 int main(int argc, char *argv[])
 {
     rclcpp::init(argc, argv);
+    cv::namedWindow("view");
+    cv::startWindowThread();
     rclcpp::spin(std::make_shared<MinimalSubscriber>());
     rclcpp::shutdown();
     return 0;
